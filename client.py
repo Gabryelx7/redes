13f03f92c0bbe11cd6c1e597a62224232d73195d
Gabryelx7 <gabrielmsd@live.com>
2025-09-30 23:13:40 -0300

    Allow multiple transfers per client

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import re
 
 BUFFER_SIZE = 2048
 NACK_BATCH_SIZE = 150
+MAX_NACK_ATTEMPS = 5
 
 # --- Tipos de mensagens de protocolo ---
 REQ = 0
@@ -13,158 +14,209 @@ NACK = 2
 INFO = 3
 ERR = 4
 ACK = 5
+BUSY = 6
 
 # --- Formato do Header ---
 HEADER_FORMAT = '!II16sB'
 HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
 
+
 def create_header(seq_num, total_segments, checksum, msg_type):
-  """Empacota os campos do header em um objeto bytes."""
-  return struct.pack(HEADER_FORMAT, seq_num, total_segments, checksum, msg_type)
+    """Empacota os campos do header em um objeto bytes."""
+    return struct.pack(HEADER_FORMAT, seq_num, total_segments, checksum, msg_type)
+
 
 def unpack_header(packet):
-  """Desempacota o header de um pacote recebido."""
-  return struct.unpack(HEADER_FORMAT, packet[:HEADER_SIZE])
+    """Desempacota o header de um pacote recebido."""
+    return struct.unpack(HEADER_FORMAT, packet[:HEADER_SIZE])
+
 
 def calculate_md5(data):
-  """Calcula o hash MD5 para um chunk de dados."""
-  return hashlib.md5(data).digest()
+    """Calcula o hash MD5 para um chunk de dados."""
+    return hashlib.md5(data).digest()
+
 
 def parse_address(user_input):
     """Processa entradas como '@127.0.0.1:9999/arquivo.txt'."""
     match = re.match(r"@([\d\.]+):(\d+)/(.+)", user_input)
     if match:
-      return match.group(1), int(match.group(2)), match.group(3)
+        return match.group(1), int(match.group(2)), match.group(3)
     return None, None, None
+
 
 def main():
     """Função main para rodar o cliente UDP"""
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Recebe o endereço do servido e o arquivo
+    # Recebe o endereço do servidor
     while True:
-      address_input = input("Digite o endereço do servidor e o arquivo (@IP:Port/filename): ")
-      server_ip, server_port, filename = parse_address(address_input)
-      if server_ip:
-        break
-      print("Formato invalido. Por favor use este formato: '@IP:Port/filename'.")
-    
+        address_input = input("Digite o endereço do servidor (@IP:Port): ")
+        match = re.match(r"@([\d\.]+):(\d+)", address_input)
+        if match:
+            server_ip, server_port = match.group(1), int(match.group(2))
+            break
+        print("Formato invalido. Por favor use este formato: '@IP:Port'.")
     server_address = (server_ip, server_port)
 
-    # Recebe simulação de perdas de pacotes
-    loss_input = input("Enter sequence numbers to drop (e.g., 5,8,12 or 'none'): ")
-    packets_to_drop = set()
-    if loss_input.lower() != 'none':
-      try:
-        packets_to_drop = {int(x.strip()) for x in loss_input.split(',')}
-      except ValueError:
-        print("Entrada inválida. Nenhum pacote será perdido.")
-    
-    # Faz a solicitação do arquivo
-    request_message = f"GET /{filename}"
-    sock.sendto(request_message.encode(), server_address)
-    print(f"\nSolicitando arquivo '{filename}' de {server_address}...")
+    while True:
+        # Recebe nome do arquivo
+        filename = input("Digite o nome do arquivo para baixar: ")
 
-    # Espera o pacote de informações
-    try:
-      sock.settimeout(5.0)
-      info_packet, _ = sock.recvfrom(BUFFER_SIZE)
-      _, total_segments, _, msg_type = unpack_header(info_packet)
-      
-      if msg_type != INFO:
-        if msg_type == ERR:
-          error_msg = info_packet[HEADER_SIZE:].decode()
-          print(f"Erro do servidor: {error_msg}")
+        # Recebe simulação de perdas de pacotes
+        loss_input = input(
+            "Digite os números de sequências a serem descartados (e.g., 5,8,12 ou 'none'): ")
+        packets_to_drop = set()
+        if loss_input.lower() != 'none':
+            try:
+                packets_to_drop = {int(x.strip())
+                                   for x in loss_input.split(',')}
+            except ValueError:
+                print("Entrada inválida. Nenhum pacote será perdido.")
+
+        # Faz a solicitação do arquivo com header
+        request_payload = f"GET /{filename}".encode()
+        request_header = create_header(0, 0, b'\x00'*16, REQ)
+        sock.sendto(request_header + request_payload, server_address)
+        print(f"\nSolicitando arquivo '{filename}' de {server_address}...")
+
+        # Espera o pacote de informações
+        while True:
+            try:
+                sock.settimeout(20.0)
+                info_packet, _ = sock.recvfrom(BUFFER_SIZE)
+                _, total_segments, _, msg_type = unpack_header(info_packet)
+
+                if msg_type != INFO:
+                    if msg_type == ERR:
+                        error_msg = info_packet[HEADER_SIZE:].decode()
+                        print(f"Erro do servidor: {error_msg}")
+                    elif msg_type == BUSY:
+                        print("Esperando liberar servidor...")
+                        continue
+                    else:
+                        print(
+                            f"Erro no tipo de pacote recebido: {msg_type}. Abortando.")
+                    break
+
+                full_file_md5 = info_packet[HEADER_SIZE:]
+                print(f"Pacote de informação recebido:")
+                print(f"- Número esperado de segmentos: {total_segments}")
+                print(f"- Hash MD5 do arquivo: {full_file_md5.hex()}")
+                break
+
+            except socket.timeout:
+                print("Servidor não respondeu à tempo")
+                break
         else:
-          print(f"Erro no tipo de pacote recebido: {msg_type}. Abortando.")
-        return  
+            continue
 
-      full_file_md5 = info_packet[HEADER_SIZE:]
-      print(f"Pacote de informação recebido:")
-      print(f"  - Número esperado de segmentos: {total_segments}")
-      print(f"  - Hash MD5 do arquivo: {full_file_md5.hex()}")
+        # Prepara para a recepção de pacotes
+        received_chunks = [None] * total_segments
+        received_count = 0
+        nack_attemps = 0
 
-    except socket.timeout:
-      print("Servidor não respondeu à tempo")
-      return
-    
-    # Prepara para a recepção de pacotes
-    received_chunks = [None] * total_segments
-    received_count = 0
+        while received_count < total_segments:
+            print("\n--- Iniciando a recepção ---")
 
-    while received_count < total_segments:
-      print("\n--- Iniciando a recepção ---")
-        
-      # Loop para receber um burst de pacotes
-      while True:
-        try:
-          # Um timeout curto indica o fim do burst
-          sock.settimeout(2.0)
-          packet, _ = sock.recvfrom(BUFFER_SIZE)
-          
-          seq_num, _, checksum, msg_type = unpack_header(packet)
-          
-          if msg_type == DATA:
-            # Simulação de perda
-            if seq_num in packets_to_drop:
-              print(f"Simulando perda do pacote {seq_num}")
-              packets_to_drop.remove(seq_num)
-              continue
+            # Guarda o progresso
+            last_received_count = received_count
 
-            # Check de integridade
-            payload = packet[HEADER_SIZE:]
-            if calculate_md5(payload) != checksum:
-              print(f"Pacote corrompido {seq_num}. Discartando.")
-              continue
-            
-            # Guarda o pacote válido
-            if received_chunks[seq_num] is None:
-              received_chunks[seq_num] = payload
-              received_count += 1
-            
-            print(f"\r{received_count}/{total_segments} segmentos recebidos", end="")
+            # Loop para receber um burst de pacotes
+            while True:
+                try:
+                    # Um timeout curto indica o fim do burst
+                    sock.settimeout(2.0)
+                    packet, _ = sock.recvfrom(BUFFER_SIZE)
 
-        except socket.timeout:
-          print("\n Burst finalizado ou pacotes perdidos. Checando por segmentos faltantes...")
-          break
-      
-      # Verifica se todos os pacotes foram recebidos
-      if received_count == total_segments:
-        print("\nTodos os segmentos foram recebidos! Verificando a integridade do arquivo...")
-        break
+                    seq_num, _, checksum, msg_type = unpack_header(packet)
 
-      # Identifica os segmentos faltantes e envia um NACK
-      missing_seqs = [str(i) for i, chunk in enumerate(received_chunks) if chunk is None]
-      if missing_seqs:
-        print(f"Número de segmentos faltantes: {len(missing_seqs)} Solicitando em lotes...")
+                    if msg_type == DATA:
+                        # Simulação de perda
+                        if seq_num in packets_to_drop:
+                            print(f"Simulando perda do pacote {seq_num}")
+                            packets_to_drop.remove(seq_num)
+                            continue
 
-        for i in range(0, len(missing_seqs), NACK_BATCH_SIZE):
-          batch = missing_seqs[i:i + NACK_BATCH_SIZE]
-          nack_payload = ",".join(batch).encode()
-          nack_header = create_header(0, 0, b'\x00'*16, NACK)
-          
-          print(f"   -> Solicitando lote de {len(batch)} segmentos começando por {batch[0]}")
-          sock.sendto(nack_header + nack_payload, server_address)
-        
-        print("Todos os Nacks foram enviados para o servidor.")
-    
-    # Monta o arquivo
-    full_data = b"".join(received_chunks)
-    
-    # Verifica novamente a integridade e escreve o resultado
-    if calculate_md5(full_data) == full_file_md5:
-      output_filename = f"received_{filename}"
-      with open(output_filename, 'wb') as f:
-        f.write(full_data)
-      print(f"Transferência do arquivo realizada com succeso! Salvo como '{output_filename}'.")
-      
-      # Envia o ACK final para o servidor
-      ack_header = create_header(0, 0, b'\x00'*16, ACK)
-      sock.sendto(ack_header, server_address)
-      print("ACK final enviado.")
-    else:
-      print("Verificação do arquivo falhou! Hashes MD5 não batem.")
+                        # Check de integridade
+                        payload = packet[HEADER_SIZE:]
+                        if calculate_md5(payload) != checksum:
+                            print(f"Pacote corrompido {seq_num}. Discartando.")
+                            continue
+
+                        # Guarda o pacote válido
+                        if received_chunks[seq_num] is None:
+                            received_chunks[seq_num] = payload
+                            received_count += 1
+
+                        print(
+                            f"\r{received_count}/{total_segments} segmentos recebidos", end="")
+
+                except socket.timeout:
+                    print("\nBurst finalizado. Checando por segmentos faltantes...")
+                    break
+
+            # Verifica se todos os pacotes foram recebidos
+            if received_count == total_segments:
+                print(
+                    "\nTodos os segmentos foram recebidos! Verificando a integridade do arquivo...")
+                break
+
+            # Identifica os segmentos faltantes e envia um NACK
+            missing_seqs = [str(i) for i, chunk in enumerate(
+                received_chunks) if chunk is None]
+            if missing_seqs:
+                # Verifica se algum progresso foi feito desde o último NACK
+                if received_count == last_received_count:
+                    nack_attemps += 1
+                    print(
+                        f"Nenhum pacote novo recebido. Tentantiva número {nack_attemps}")
+                else:
+                    nack_attemps = 0
+
+                # Verifica se já fizemos o máximo de NACKS
+                if nack_attemps >= MAX_NACK_ATTEMPS:
+                    print('Servidor não está respondendo. Abortando transferência')
+                    break
+
+                print(
+                    f"Número de segmentos faltantes: {len(missing_seqs)}. Solicitando em lotes...")
+
+                for i in range(0, len(missing_seqs), NACK_BATCH_SIZE):
+                    batch = missing_seqs[i:i + NACK_BATCH_SIZE]
+                    nack_payload = ",".join(batch).encode()
+                    nack_header = create_header(0, 0, b'\x00'*16, NACK)
+
+                    print(
+                        f"-- Solicitando lote de {len(batch)} segmentos começando por {batch[0]}")
+                    sock.sendto(nack_header + nack_payload, server_address)
+
+                print("Todos os Nacks foram enviados para o servidor.")
+
+        # Monta o arquivo
+        full_data = b"".join(received_chunks)
+
+        # Verifica novamente a integridade e escreve o resultado
+        if calculate_md5(full_data) == full_file_md5:
+            output_filename = f"received_{filename}"
+            with open(output_filename, 'wb') as f:
+                f.write(full_data)
+            print(
+                f"Transferência do arquivo realizada com succeso! Salvo como '{output_filename}'.")
+
+            # Envia o ACK final para o servidor
+            ack_header = create_header(0, 0, b'\x00'*16, ACK)
+            sock.sendto(ack_header, server_address)
+            print("ACK final enviado.")
+        else:
+            print("Verificação do arquivo falhou! Hashes MD5 não batem.")
+
+        # Prompt para nova transferência
+        again = input("Deseja baixar outro arquivo? (s/n): ").strip().lower()
+        if again != 's':
+            print("Encerrando cliente.")
+            break
+
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import math
 HOST = '0.0.0.0'
 PORT = 9999
 BUFFER_SIZE = 2048
-PAYLOAD_SIZE = 1400 # MTU = 1500 bytes
+PAYLOAD_SIZE = 1400  # MTU = 1500 bytes
 
 # --- Tipos de mensagens de protocolo ---
 REQ = 0
@@ -18,125 +18,166 @@ ERR = 4
 ACK = 5
 BUSY = 6
 
-ACTIVE_CLIENT = None
-
 # --- Formato do Header ---
-# ! = Ordenação big-endian para rede
-# I = unsigned int (4 bytes)
-# 16s = string de 16 bytes (para o MD5)
-# B = unsigned char (1 byte)
+# !   = Ordenação big-endian para rede
+# I   = unsigned int (4 bytes)          - Número da sequência
+# I   = unsigned int (4 bytes)          - Número total de segmentos
+# 16s = string de 16 bytes              - Hash MD5
+# B   = unsigned char (1 byte)          - Tipo de mensagem
 HEADER_FORMAT = '!II16sB'
-HEADER_SIZE = struct.calcsize(HEADER_FORMAT) # 25 Bytes
+HEADER_SIZE = struct.calcsize(HEADER_FORMAT)  # 25 Bytes
+
 
 def create_header(seq_num, total_segments, checksum, msg_type):
-  """Empacota os campos do header em um objeto de bytes."""
-  return struct.pack(HEADER_FORMAT, seq_num, total_segments, checksum, msg_type)
+    """Empacota os campos do header em um objeto de bytes."""
+    return struct.pack(HEADER_FORMAT, seq_num, total_segments, checksum, msg_type)
+
 
 def unpack_header(packet):
-  """Desempacota o header de um pacote recebido."""
-  return struct.unpack(HEADER_FORMAT, packet[:HEADER_SIZE])
+    """Desempacota o header de um pacote recebido."""
+    return struct.unpack(HEADER_FORMAT, packet[:HEADER_SIZE])
+
 
 def calculate_md5(data):
-  """Calcula o hash MD5 para um chunk de dados."""
-  return hashlib.md5(data).digest()
+    """Calcula o hash MD5 para um chunk de dados."""
+    return hashlib.md5(data).digest()
+
 
 def main():
-  """Função main para rodar o servidor UDP."""
+    """Função main para rodar o servidor UDP."""
 
-  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  sock.bind((HOST, PORT))
-  print(f"Servidor escutando em {HOST}:{PORT}")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((HOST, PORT))
+    print(f"Servidor escutando em {HOST}:{PORT}")
 
-  while True:
-    print("\nEsperando por um novo cliente...")
-    try:
-      # Espera pela requisição de arquivo
-      request, client_address = sock.recvfrom(BUFFER_SIZE)
-      
-      # Supõe que a solicitação está no formato "GET /filename.ext"
-      filename = request.decode().strip().split(' ')[1][1:]
-      print(f"solicitação de arquivo '{filename}' recebida de {client_address}")
+    busy = False
+    waiting_clients = []
 
-      # Verifica se o arquivo existe
-      if not os.path.exists(filename):
-        print(f"Arquivo não encontrado: {filename}")
-        error_header = create_header(0, 0, b'\x00'*16, ERR)
-        error_message = b"Arquivo nao encontrado"
-        sock.sendto(error_header + error_message, client_address)
-        continue
-
-      # Lê o arquivo existente
-      with open(filename, 'rb') as f:
-        file_content = f.read()
-
-      file_size = len(file_content)
-      total_segments = math.ceil(file_size / PAYLOAD_SIZE)
-      full_file_md5 = calculate_md5(file_content)
-      
-      print(f"  - Tamanho do arquivo: {file_size / 1024:.2f} KB")
-      print(f"  - Número de segmentos: {total_segments}")
-      print(f"  - Hash MD5: {full_file_md5.hex()}")
-
-      # Manda um pacote de informoções com os metadados (header e hash MD5)
-      info_header = create_header(0, total_segments, b'\x00'*16, INFO)
-      sock.sendto(info_header + full_file_md5, client_address)
-      print("Pacote de metadados enviado para o cliente.")
-
-      # Manda todos os pacotes de dados
-      print("Iniciando transferência do arquivo...")
-      for i in range(total_segments):
-        start = i * PAYLOAD_SIZE
-        end = start + PAYLOAD_SIZE
-        chunk = file_content[start:end]
-        chunk_md5 = calculate_md5(chunk)
-        
-        data_header = create_header(i, total_segments, chunk_md5, DATA)
-        sock.sendto(data_header + chunk, client_address)
-
-      print("Transferência completada.")
-
-      # Lida com o processo de retransmissão dos arquivos
-      while True:
+    while True:
+        print("\nEsperando por um novo cliente...")
         try:
-          sock.settimeout(None)
-          response, sender_address = sock.recvfrom(BUFFER_SIZE)
+            # Se houver clientes esperando, processa imediatamente
+            if waiting_clients:
+                print(f"Processando cliente em espera...")
+                request, client_address = waiting_clients.pop(0)
+            else:
+                sock.settimeout(2.0 if busy else None)
+                request, client_address = sock.recvfrom(BUFFER_SIZE)
 
-          if sender_address != client_address:
-            print(f"  -- Ignorando pacotes de fonte inesperada: {sender_address}")
-            continue
-          
-          try:
-            _, _, _, msg_type = unpack_header(response)
-          except struct.error:
-            print(f"  -- Ignorando pacote! --")
+            if busy:
+                print(
+                    f"Cliente {client_address} tentou conectar enquanto servidor ocupado. Adicionando à fila de espera.")
+                waiting_clients.append((request, client_address))
+                # Mensagem para o cliente aguardando
+                wait_header = create_header(0, 0, b'\x00'*16, INFO)
+                wait_msg = b"Servidor ocupado, aguarde..."
+                sock.sendto(wait_header + wait_msg, client_address)
+                continue
 
-          if msg_type == NACK:
-            # O cliente está requerindo retransmissões
-            missing_seqs_str = response[HEADER_SIZE:].decode()
-            missing_seqs = [int(s) for s in missing_seqs_str.split(',')]
-            #print(f"NACK recebido para os segmentos: {missing_seqs}")
+            busy = True
+            # Supõe que a solicitação está no formato "GET /filename.ext"
+            filename = request.decode().strip().split(' ')[1][1:]
+            print(
+                f"solicitação de arquivo '{filename}' recebida de {client_address}")
 
-            for seq_num in missing_seqs:
-              start = seq_num * PAYLOAD_SIZE
-              end = start + PAYLOAD_SIZE
-              chunk = file_content[start:end]
-              chunk_md5 = calculate_md5(chunk)
-              
-              data_header = create_header(seq_num, total_segments, chunk_md5, DATA)
-              sock.sendto(data_header + chunk, client_address)
-            print(f"{len(missing_seqs)} pacotes foram reenviados.")
+            # Verifica se o arquivo existe
+            if not os.path.exists(filename):
+                print(f"Arquivo não encontrado: {filename}")
+                error_header = create_header(0, 0, b'\x00'*16, ERR)
+                error_message = b"Arquivo nao encontrado"
+                sock.sendto(error_header + error_message, client_address)
+                busy = False
+                continue
 
-          elif msg_type == ACK:
-            # Cliente confirmou a transferência
-            print(f"O cliente {client_address} confirmou a transferência com sucesso de: '{filename}'.")
-            break
+            # Lê o arquivo existente
+            with open(filename, 'rb') as f:
+                file_content = f.read()
 
+            file_size = len(file_content)
+            total_segments = math.ceil(file_size / PAYLOAD_SIZE)
+            full_file_md5 = calculate_md5(file_content)
+
+            print(f"\n- Tamanho do arquivo: {file_size / 1024:.2f} KB")
+            print(f"- Número de segmentos: {total_segments}")
+            print(f"- Hash MD5: {full_file_md5.hex()}")
+
+            # Manda um pacote de informoções com os metadados (header e hash MD5)
+            info_header = create_header(0, total_segments, b'\x00'*16, INFO)
+            sock.sendto(info_header + full_file_md5, client_address)
+            print("\nPacote de metadados enviado para o cliente.")
+
+            # Manda todos os pacotes de dados
+            print("\nIniciando transferência do arquivo...")
+            for i in range(total_segments):
+                start = i * PAYLOAD_SIZE
+                end = start + PAYLOAD_SIZE
+                chunk = file_content[start:end]
+                chunk_md5 = calculate_md5(chunk)
+
+                data_header = create_header(i, total_segments, chunk_md5, DATA)
+                sock.sendto(data_header + chunk, client_address)
+
+            print("Transferência completada.")
+
+            # Lida com o processo de retransmissão dos arquivos
+            while True:
+                try:
+                    sock.settimeout(10.0)
+                    response, sender_address = sock.recvfrom(BUFFER_SIZE)
+
+                    # Verifica se é uma nova requisição de outro cliente
+                    try:
+                        _, _, _, msg_type = unpack_header(response)
+                    except struct.error:
+                        print(f"-- Ignorando pacote! --")
+                        continue
+
+                    if sender_address != client_address:
+                        if msg_type == REQ:
+                            print(
+                                f"Cliente {sender_address} tentou conectar enquanto servidor ocupado (durante retransmissão). Adicionando à fila de espera.")
+                            waiting_clients.append((response, sender_address))
+                            wait_header = create_header(0, 0, b'\x00'*16, BUSY)
+                            wait_msg = b"Servidor ocupado, aguarde..."
+                            sock.sendto(wait_header + wait_msg, sender_address)
+                        else:
+                            print(
+                                f"-- Ignorando pacotes de fonte inesperada: {sender_address}")
+                        continue
+
+                    if msg_type == NACK:
+                        # O cliente está requerindo retransmissões
+                        missing_seqs_str = response[HEADER_SIZE:].decode()
+                        missing_seqs = [int(s)
+                                        for s in missing_seqs_str.split(',')]
+                        # print(f"NACK recebido para os segmentos: {missing_seqs}")
+
+                        for seq_num in missing_seqs:
+                            start = seq_num * PAYLOAD_SIZE
+                            end = start + PAYLOAD_SIZE
+                            chunk = file_content[start:end]
+                            chunk_md5 = calculate_md5(chunk)
+
+                            data_header = create_header(
+                                seq_num, total_segments, chunk_md5, DATA)
+                            sock.sendto(data_header + chunk, client_address)
+                        print(
+                            f"\n{len(missing_seqs)} pacotes foram reenviados.")
+
+                    elif msg_type == ACK:
+                        # Cliente confirmou a transferência
+                        print(
+                            f"\nO cliente {client_address} confirmou a transferência com sucesso de: '{filename}'.")
+                        break
+
+                except socket.timeout:
+                    print(
+                        f"\nO cliente {client_address} expirou. Encerrando a conexão.")
+                    break
+            busy = False
         except socket.timeout:
-          print(f"O cliente {client_address} expirou. Encerrando a conexão.")
-          break
-    except socket.timeout:
-      continue    
+            continue
 
 
 if __name__ == "__main__":
-  main()
+    main()
